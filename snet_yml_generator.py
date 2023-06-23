1694fbeae97d3d3fa89a05f71f7259b342f9aa73
Dadaism7 <136952919+Dadaism7@users.noreply.github.com>
2023-06-23 08:51:28 +0000

    Deploying to gh-pages from @ Dadaism7/Dadaism7.github.io@0f2308069e4126ab2da96a098efe6c7c65806a81 🚀

--- snet_yml_generator.py
+++ snet_yml_generator.py
@@ -1,24 +1,66 @@
 import os
+import urllib.parse
 
 # List of directories to scan for images
-directories = ["assets/img/nuplan/new", 
-           "assets/img/pg/new", 
-           "assets/img/waymo/new"]
+directories = ["assets/scenarionet/nuplan-1-compressed/", "assets/scenarionet/nuplan-2-compressed/", "assets/scenarionet/waymo-1-compressed/","assets/scenarionet/waymo-2-compressed/"]
+# src_prepend = "https://github.com/Dadaism6/metadriverse-asset/raw/main/"
 
 # Ensure _images directory exists
-os.makedirs('_images', exist_ok=True)
+os.makedirs('_snet-asset', exist_ok=True)
 
 for directory in directories:
     for root, dirs, files in os.walk(directory):
         for file in files:
-            if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif')):
+            if file.lower().endswith(('.mp4')):
                 src = "/" + os.path.join(root, file) 
                 alt = os.path.splitext(file)[0]  # Use the filename without extension as alt text
                 filename = os.path.splitext(file)[0] + '.md'  # Markdown filename
 
                 # Create Markdown file in _images directory
-                with open(os.path.join('_images', filename), 'w') as f:
+                with open(os.path.join('_snet-asset', filename), 'w') as f:
                     f.write(f'---\nsrc: {src}\nalt: {alt}\n---\n')
+
+
+
+
+
+# # List of directories to scan for images
+# directories = ["/Users/duanchenda/Downloads/scenarionet/nuplan-1/"]
+# # src_prepend = "https://github.com/Dadaism6/metadriverse-asset/raw/main/"
+
+# # Ensure _images directory exists
+# os.makedirs('_images', exist_ok=True)
+
+# for directory in directories:
+#     for root, dirs, files in os.walk(directory):
+#         for file in files:
+#             if file.lower().endswith(('.mp4')):
+#                 src = urllib.parse.quote(file)  # URL-encode the filename
+#                 alt = os.path.splitext(file)[0]  # Use the filename without extension as alt text
+#                 filename = os.path.splitext(file)[0] + '.md'  # Markdown filename
+
+#                 # Create Markdown file in _images directory
+#                 with open(os.path.join('_images', filename), 'w') as f:
+#                     f.write(f'---\nsrc: {src_prepend}{src}\nalt: {alt}\n---\n')
+
+
+# # List of directories to scan for images
+# directories = ["/Users/duanchenda/Downloads/scenarionet/nuplan-1/"]
+
+# # Ensure _images directory exists
+# os.makedirs('_images', exist_ok=True)
+
+# for directory in directories:
+#     for root, dirs, files in os.walk(directory):
+#         for file in files:
+#             if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif')):
+#                 src = "/" + os.path.join(root, file) 
+#                 alt = os.path.splitext(file)[0]  # Use the filename without extension as alt text
+#                 filename = os.path.splitext(file)[0] + '.md'  # Markdown filename
+
+#                 # Create Markdown file in _images directory
+#                 with open(os.path.join('_images', filename), 'w') as f:
+#                     f.write(f'---\nsrc: {src}\nalt: {alt}\n---\n')
 
 # import os
 # import yaml
