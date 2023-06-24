8db08d39b4a6910ffc1a0240630a300be944d067
Dadaism7 <136952919+Dadaism7@users.noreply.github.com>
2023-06-24 04:46:22 +0000

    Deploying to gh-pages from @ Dadaism7/Dadaism7.github.io@42ce7b5f0eab582729c87ad56c62c708a18723c6 🚀

--- snet_issue_yml.py
+++ snet_issue_yml.py
@@ -1,25 +1,21 @@
 import os
-import urllib.parse
-
-
-directories = ["assets/scenarionet/nuplan-1-compressed/",]
-src_prepend = "https://github.com/Dadaism6/metadriverse-asset/releases/download/assetsv1.0.1/"
-
-# Ensure _images directory exists
 os.makedirs('_snet-asset', exist_ok=True)
+def create_markdown_files(filename):
+    with open(filename, 'r') as f:
+        lines = f.read().splitlines()
+        
+    for i, link in enumerate(lines):
+        if len(link) != 0:
+            md_content = f"""---
+src: {link}
+alt: asset-{i+1}
+---"""
+            with open(os.path.join('_snet-asset', f'asset-{i+1}.md'), 'w') as md_file:
+                md_file.write(md_content)
 
-for directory in directories:
-    for root, dirs, files in os.walk(directory):
-        for file in files:
-            if file.lower().endswith(('.mp4')):
-                # src = "/" + os.path.join(root, file) 
-                src = file
-                alt = os.path.splitext(file)[0]  # Use the filename without extension as alt text
-                filename = os.path.splitext(file)[0] + '.md'  # Markdown filename
+# Run the function with your filename
+create_markdown_files("issuelink.txt")
 
-                # Create Markdown file in _images directory
-                with open(os.path.join('_snet-asset', filename), 'w') as f:
-                    f.write(f'---\nsrc: {src_prepend}{src}\nalt: {alt}\n---\n')
 
 
 
