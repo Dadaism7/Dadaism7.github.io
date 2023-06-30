b7efadab5b5a0599fc757622a9bb153304d806e7
Dadaism7 <136952919+Dadaism7@users.noreply.github.com>
2023-06-30 16:41:29 +0000

    Deploying to gh-pages from @ Dadaism7/Dadaism7.github.io@6c75adc4ff97df915b2be18d592585c121d95821 🚀

--- snet_yml_generator.py
+++ snet_yml_generator.py
@@ -1,12 +1,15 @@
 import os
 import urllib.parse
-
-
-directories = ["assets/scenarionet/nuplan-1-compressed/",]
-src_prepend = "https://github.com/Dadaism6/metadriverse-asset/releases/download/assetsv1.0.1/"
+directories = ["assets/scenarionet/nuplan-1-newcompressed/", "assets/scenarionet/nuplan-2-newcompressed/", "assets/scenarionet/waymo-1-newcompressed/","assets/scenarionet/waymo-2-newcompressed/"]
+src_prepend = "https://github.com/Dadaism6/metadriverse-asset/releases/download/assetsv1.0.2/"
 
 # Ensure _images directory exists
 os.makedirs('_snet-asset', exist_ok=True)
+
+# Create a dictionary to store the current order for each directory
+orders = {}
+for directory in directories:
+    orders[directory] = 0
 
 for directory in directories:
     for root, dirs, files in os.walk(directory):
@@ -16,10 +19,65 @@ for directory in directories:
                 src = file
                 alt = os.path.splitext(file)[0]  # Use the filename without extension as alt text
                 filename = os.path.splitext(file)[0] + '.md'  # Markdown filename
+                
+                # Split the filename into tag and id
+                split_name = os.path.splitext(file)[0].split("_") 
+                tag = split_name[0]
+                video_id = split_name[1]
 
                 # Create Markdown file in _images directory
                 with open(os.path.join('_snet-asset', filename), 'w') as f:
-                    f.write(f'---\nsrc: {src_prepend}{src}\nalt: {alt}\n---\n')
+                    f.write(f'---\nsrc: {src_prepend}{src}\nalt: {alt}\ntag: {tag}\nvid: {video_id}\norder: {orders[directory]}\n---\n')
+                    
+                # Increase the order by the number of directories
+                orders[directory] += len(directories)
+
+
+
+# directories = ["assets/scenarionet/nuplan-1-newcompressed/", "assets/scenarionet/nuplan-2-newcompressed/", "assets/scenarionet/waymo-1-newcompressed/","assets/scenarionet/waymo-2-newcompressed/"]
+# src_prepend = "https://github.com/Dadaism6/metadriverse-asset/releases/download/assetsv1.0.2/"
+
+# # Ensure _images directory exists
+# os.makedirs('_snet-asset', exist_ok=True)
+
+# for directory in directories:
+#     for root, dirs, files in os.walk(directory):
+#         for file in files:
+#             if file.lower().endswith(('.mp4')):
+#                 # src = "/" + os.path.join(root, file) 
+#                 src = file
+#                 alt = os.path.splitext(file)[0]  # Use the filename without extension as alt text
+#                 filename = os.path.splitext(file)[0] + '.md'  # Markdown filename
+                
+#                 # Split the filename into tag and id
+#                 split_name = os.path.splitext(file)[0].split("_") 
+#                 tag = split_name[0]
+#                 video_id = split_name[1]
+
+#                 # Create Markdown file in _images directory
+#                 with open(os.path.join('_snet-asset', filename), 'w') as f:
+#                     f.write(f'---\nsrc: {src_prepend}{src}\nalt: {alt}\ntag: {tag}\nvid: {video_id}\n---\n')
+
+
+
+# directories = ["assets/scenarionet/nuplan-1-compressed/", "assets/scenarionet/nuplan-2-compressed/", "assets/scenarionet/waymo-1-compressed/","assets/scenarionet/waymo-2-compressed/"]
+# src_prepend = "https://github.com/Dadaism6/metadriverse-asset/releases/download/assetsv1.0.1/"
+
+# # Ensure _images directory exists
+# os.makedirs('_snet-asset', exist_ok=True)
+
+# for directory in directories:
+#     for root, dirs, files in os.walk(directory):
+#         for file in files:
+#             if file.lower().endswith(('.mp4')):
+#                 # src = "/" + os.path.join(root, file) 
+#                 src = file
+#                 alt = os.path.splitext(file)[0]  # Use the filename without extension as alt text
+#                 filename = os.path.splitext(file)[0] + '.md'  # Markdown filename
+
+#                 # Create Markdown file in _images directory
+#                 with open(os.path.join('_snet-asset', filename), 'w') as f:
+#                     f.write(f'---\nsrc: {src_prepend}{src}\nalt: {alt}\n---\n')
 
 
 
